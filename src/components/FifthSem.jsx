bab405a63c69904952d613651a1b4c9eaa6f1a04
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-08-27 13:15:18 +0530

    fix study material layout

--- src/components/FifthSem.jsx
+++ src/components/FifthSem.jsx
@@ -8,8 +8,8 @@ const FifthSem = () => {
     <>
       <Header />
       <div className="p-4 min-h-screen max-w-7xl mx-auto">
-      <motion.h2
-          className="text-5xl md:text-7xl flex justify-center font-bold mb-4"
+        <motion.h2
+          className="text-3xl md:text-7xl flex justify-center font-bold mb-4"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -17,7 +17,7 @@ const FifthSem = () => {
           Study Material
         </motion.h2>
         <motion.h2
-          className="text-2xl md:text-3xl flex justify-center font-bold mb-12"
+          className="text-md md:text-3xl flex justify-center font-bold mb-4"
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
@@ -26,13 +26,34 @@ const FifthSem = () => {
         </motion.h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {[
-            { name: "MST & PTU PYQs", link: "https://drive.google.com/drive/folders/1Dq8UveJ4bGwv6IA7rWivQ9E4ShT5Upi1?usp=share_link" },
-            { name: "Computer Networks", link: "https://drive.google.com/drive/folders/1-TdEbR584WFzqaKGt0nGzh0M8EFcc7SK?usp=sharing" },
-            { name: "Database Management Systems", link: "https://drive.google.com/drive/folders/16PDIUdIsXHsREsjUoAuIqXv79lhhrPVQ?usp=sharing" },
-            { name: "Software Engineering", link: "https://drive.google.com/drive/folders/16D7ev_L66LfXHN9tN7ZtRcXYPduaK9nP?usp=share_link" },
-            { name: "Web Technology", link: "https://drive.google.com/drive/folders/183utImCAGPQ9IoMnBYFMK9gJph_AXg12?usp=share_link" },
-            { name: "FLAT", link: "https://drive.google.com/drive/folders/16R98ur8cM98AaYo8PRJ6BAnzUo7nGN0m?usp=share_link" },
-            { name: "COI", link: "https://drive.google.com/drive/folders/13J_-edqSaBq_zdMmbLCSvLr7-55fcizm?usp=sharing" },
+            {
+              name: "MST & PTU PYQs",
+              link: "https://drive.google.com/drive/folders/1Dq8UveJ4bGwv6IA7rWivQ9E4ShT5Upi1?usp=share_link",
+            },
+            {
+              name: "Computer Networks",
+              link: "https://drive.google.com/drive/folders/1-TdEbR584WFzqaKGt0nGzh0M8EFcc7SK?usp=sharing",
+            },
+            {
+              name: "Database Management Systems",
+              link: "https://drive.google.com/drive/folders/16PDIUdIsXHsREsjUoAuIqXv79lhhrPVQ?usp=sharing",
+            },
+            {
+              name: "Software Engineering",
+              link: "https://drive.google.com/drive/folders/16D7ev_L66LfXHN9tN7ZtRcXYPduaK9nP?usp=share_link",
+            },
+            {
+              name: "Web Technology",
+              link: "https://drive.google.com/drive/folders/183utImCAGPQ9IoMnBYFMK9gJph_AXg12?usp=share_link",
+            },
+            {
+              name: "FLAT",
+              link: "https://drive.google.com/drive/folders/16R98ur8cM98AaYo8PRJ6BAnzUo7nGN0m?usp=share_link",
+            },
+            {
+              name: "COI",
+              link: "https://drive.google.com/drive/folders/13J_-edqSaBq_zdMmbLCSvLr7-55fcizm?usp=sharing",
+            },
           ].map((item, index) => (
             <motion.div
               key={index}
